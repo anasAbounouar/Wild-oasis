32422cff8ecd32b2bb485aff917466c2d329f174
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-02-01 14:05:26 +0100

    detected click outside modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,33 +1,51 @@
-import { useState } from "react";
+
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
-
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  const btnStyle = {
-    width: "100%",
-
-    marginTop: "10px",
-    marginBottom: "10px",
-  };
   return (
-    <>
-      <Button
-        style={btnStyle}
-        size="large"
-        variation="primary"
-        onClick={() => setIsOpenModal((prevState) => !prevState)}
-      >
-        {isOpenModal ? "cancel" : "add new cabin"}
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button variation='primary' size='medium'>add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+      {/* <Modal.Open opens="table">
+        <Button>add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CreateCabinForm />
+      </Modal.Window> */}
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   const btnStyle = {
+//     width: "100%",
+
+//     marginTop: "10px",
+//     marginBottom: "10px",
+//   };
+//   return (
+//     <>
+//       <Button
+//         style={btnStyle}
+//         size="large"
+//         variation="primary"
+//         onClick={() => setIsOpenModal((prevState) => !prevState)}
+//       >
+//         {isOpenModal ? "cancel" : "add new cabin"}
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// }
 
 export default AddCabin;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,15 @@
-import { createContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -52,22 +60,54 @@ const Button = styled.button`
 `;
 
 const ModalContext = createContext();
-
 function Modal({ children }) {
-  const [openName, setOpenName] = useState();
+  const [openName, setOpenName] = useState("");
+  const close = () => setOpenName("");
+  const open = (newName) => setOpenName(newName);
+  return (
+    <ModalContext.Provider
+      value={{
+        openName,
+        open,
+        close,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
 }
-function Window({ children, onClose }) {
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+  return cloneElement(children, {
+    onClick: () => {
+      open(opensWindowName);
+    },
+  });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const  ref  = useOutsideClick(close)
+
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
-        <Button onClick={onClose}>
+      <StyledModal ref={ref}>
+        <Button onClick={() => close()}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body,
   );
 }
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
+
+// function Modal({ children }) {
+//   const [openName, setOpenName] = useState();
+// }
