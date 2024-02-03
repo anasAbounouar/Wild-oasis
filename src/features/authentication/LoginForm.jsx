657783939f9a7a2c361fd2abf04732136d567455
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-02-03 22:08:49 +0100

    athentication (so far without authorization)

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,17 +3,25 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("anass.abounouar@gmail.com");
+  const [password, setPassword] = useState("espagne");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormRowVertical label="Email address">
         <Input
+          disabled={isLoading}
           type="email"
           id="email"
           // This makes this form better for password managers
@@ -24,6 +32,7 @@ function LoginForm() {
       </FormRowVertical>
       <FormRowVertical label="Password">
         <Input
+          disabled={isLoading}
           type="password"
           id="password"
           autoComplete="current-password"
@@ -32,7 +41,9 @@ function LoginForm() {
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button disabled={isLoading} variation="primary" size="large">
+          {isLoading ? <SpinnerMini /> : "login"}
+        </Button>
       </FormRowVertical>
     </Form>
   );
