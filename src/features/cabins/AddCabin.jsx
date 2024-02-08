1db3b816c8a5d15b942d0032430d81fa28fc0979
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-02-08 22:00:16 +0100

    debugging

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -13,42 +13,10 @@ function AddCabin() {
         <Modal.Window name="cabin-form">
           <CreateCabinForm />
         </Modal.Window>
-        {/* <Modal.Open opens="table">
-        <Button>add new Cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-      <CreateCabinForm />
-    </Modal.Window> */}
+       
       </Modal>
     </div>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-//   const btnStyle = {
-//     width: "100%",
-
-//     marginTop: "10px",
-//     marginBottom: "10px",
-//   };
-//   return (
-//     <>
-//       <Button
-//         style={btnStyle}
-//         size="large"
-//         variation="primary"
-//         onClick={() => setIsOpenModal((prevState) => !prevState)}
-//       >
-//         {isOpenModal ? "cancel" : "add new cabin"}
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </>
-//   );
-// }
 
 export default AddCabin;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,7 +2,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import CabinTable from "../features/cabins/CabinTable";
-import AddCabin from "../features/cabins/addCabin";
+import AddCabin from "../features/cabins/AddCabin";
 import CabinTableOperations from "../features/cabins/cabinTableOperations";
 
 function Cabins() {
